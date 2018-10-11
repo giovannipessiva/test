4573183bf8e19d10a9e4084d5e2eab1f83bd8585
rpt <rpt@altervista.org>
2018-10-12 00:19:20 +0200

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
 	entry: [
-		"./client/dist/Client.ts",
-		"./client/dist/Client2.ts"
+		".\\client\\dist\\test\\client\\src\\Client.js",
+		".\\client\\dist\\test\\client\\src\\Client2.js"
 	],
 	output: {
+		library: "Test123",
 		path: path.resolve(__dirname, "client/dist"), // path: "./client/dist"
 	}
 }
