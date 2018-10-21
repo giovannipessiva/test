72faa0f1c8f4bfce57e31e926cf555506f850e2f
rpt <rpt@altervista.org>
2018-10-21 20:41:55 +0200

    fix ts-loader

--- client/dist/test/client/src/Client.js
+++
@@ -1,5 +0,0 @@
-import { Client2 } from "./Client2";
-export function client() {
-    console.log("1 " + Client2.client());
-    console.log("2 " + Client2.client2());
-}
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,28 @@ const path = require('path');
 module.exports = {
 	mode: "production",
 	entry: [
-		"./client/dist/test/client/src/Client.js",
-		"./client/dist/test/client/src/Client2.js"
+		"./client/src/Client.ts"
 	],
-	devtool: "inline-source-map",
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: {
-					loader: "ts-loader",
-					options: {
-						configFile: "./client/tsconfig-client.json"
-					}
-				}
-			}
-		]
-	},
 	output: {
 		filename: "test123.webpacked.js",
 		library: "Test123",
 		path: path.resolve(__dirname, "client/dist"), // path: "./client/dist"
+	},
+	devtool: "inline-source-map",
+	resolve: {
+        extensions: [ ".ts", ".js" ]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: [/node_modules/],
+				loader: "ts-loader",
+				options: {
+					configFile: "client/tsconfig-client.json",
+					logLevel: "info"
+				}
+			}
+		]
 	}
 }
